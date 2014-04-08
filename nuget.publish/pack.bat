341adc4c52844e6b4feaf36996451b3fa2f889dd
Denis Gladkikh <outcoldman@gmail.com>
2014-04-08 12:13:29 -0700

    Universal support

--- nuget.publish/pack.bat
+++ nuget.publish/pack.bat
@@ -1,4 +1,2 @@
 msbuild.exe ..\SQLite.sln /property:Configuration=Release /m 
-sn -R "..\WindowsStore\bin\Release\OutcoldSolutions.SQLite.dll" %OutcoldTools%\OutcoldSolutions.snk
-sn -R "..\Windows\bin\Release\OutcoldSolutions.SQLite.dll" %OutcoldTools%\OutcoldSolutions.snk
-nuget pack "OutcoldSolutions.SQLite.nuspec" -basepath "..\\"
+.\..\nuget\nuget pack "OutcoldSolutions.SQLite.nuspec" -basepath "..\\"
